e11257e271c7ea8b3b615e7f391076d937e5b1a3
Makoge Boris <makogeboris@gmail.com>
2023-09-04 23:37:32 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      StrongCyan: "hsl(171, 66%, 44%)",
+      LightBlue: "hsl(233, 100%, 69%)",
+      DarkGrayishBlue: "hsl(210, 10%, 33%)",
+      GrayishBlue: "hsl(201, 11%, 66%)",
+    },
+    fontFamily: {
+      sans: ["Bai Jamjuree", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
